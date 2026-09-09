049ce96c70a8a6a482fa57ace42c672181e6b18c
Sandeep-raj <31364606+Sandeep-raj@users.noreply.github.com>
2026-09-09 20:18:27 +0530

    Create pureStorage3.go

---
+++ LeetCode/pureStorage3.go
@@ -0,0 +1,141 @@
+package main;
+
+import (
+  "fmt"
+  "time"
+  "sync"
+  "runtime"
+)
+
+func getgoid() {
+  return runtime.GoID()
+}
+
+type cb func();
+type PriorityLock struct {
+  mu *sync.Mutex
+  writerWaiting bool
+  readerCount int
+  cond *sync.Cond
+  idmap map[uint64]int
+}
+
+func createLock() PriorityLock {
+  m := &sync.Mutex{}
+  return PriorityLock{
+    mu : m,
+    cond : sync.NewCond(m),
+    idmap : make(map[uint64]int),
+  }
+}
+
+var lock PriorityLock = createLock()
+
+func (l *PriorityLock) RLock() {
+  l.mu.Lock()
+
+  id := getgoid()
+  if l.idmap[id] > 0 {
+    l.idmap[id]++;
+    l.readerCount++;
+    l.mu.Unlock()
+    return;
+  }
+
+  for l.writerWaiting {
+    l.cond.Wait()
+  }
+  l.readerCount++;
+  l.mu.Unlock()
+}
+
+func (l *PriorityLock) RUnlock() {
+  l.mu.Lock()
+
+  id := getgoid()
+  
+  l.idmap[id]--;
+  l.readerCount--;
+
+  if l.idmao[id] == 0 {
+    delete(l.idmap, id)
+  }
+  
+  if l.readerCount == 0 && l.writerWaiting {
+    l.cond.Broadcast()
+  }
+  l.mu.Unlock()
+}
+
+func (l *PriorityLock) Lock() {
+  l.mu.Lock()
+  l.writerWaiting = true
+  
+  for l.readerCount > 0 {
+    l.cond.Wait()
+  }
+}
+
+func (l *PriorityLock) Unlock() {
+  l.writerWaiting = false
+  l.cond.Broadcast()
+  l.mu.Unlock()
+}
+
+func regCb(fn cb) {
+  lock.RLock()
+  defer lock.RUnlock()
+  fmt.Println("cb is executing");
+  fn();
+  fmt.Println("cb is completed");
+}
+
+func execEvents() {
+  lock.Lock()
+  defer lock.Unlock()
+  fmt.Println("event is executing");
+  time.Sleep(5 * time.Second);
+  fmt.Println("event is completed");
+}
+
+func main() {
+  wg := sync.WaitGroup{}
+
+  wg.Add(1)
+  go func() {
+    defer wg.Done()
+    regCb(func() {
+      time.Sleep(2 * time.Second);
+      regCb(func() {
+        fmt.Println("this is test")
+      })
+    })
+  }()
+  time.Sleep(100 * time.Millisecond)
+  wg.Add(1)
+  go func() {
+    defer wg.Done()
+    execEvents()
+  }()
+
+  time.Sleep(1 * time.Second)
+  wg.Add(1)
+  go func() {
+    defer wg.Done()
+    regCb(func() {
+      time.Sleep(2 * time.Second);
+    })
+  }()
+
+  time.Sleep(2 * time.Second)
+  wg.Add(1)
+  go func() {
+    defer wg.Done()
+    regCb(func() {
+      time.Sleep(2 * time.Second);
+    })
+  }()
+
+  wg.Wait()
+  fmt.Println("Workflow Completed")
+}
